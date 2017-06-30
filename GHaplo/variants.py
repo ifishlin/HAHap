407bca859e4e5fb42998e3a099f600e63b5f2afb
ifishlin324 <ifishlin324@gmail.com>
2017-06-30 22:41:18 +0800

    faster discover allele on reads

--- GHaplo/variants.py
+++ GHaplo/variants.py
@@ -8,15 +8,6 @@ import pysam
 import numpy as np
 from scipy.sparse import csr_matrix, lil_matrix
 
-V_DESC_CRHOM  = 0
-V_DESC_LOC    = 1
-V_DESC_ExtLen = 2
-V_DESC_TYPE   = 3
-V_DESC_ALLELE = 4
-V_DESC_REFMAJ   = 5
-V_DESC_CALLEDH1 = 6
-V_DESC_CALLEDH2 = 7
-
 class InputVcfReader(object):
     """
     """
@@ -31,45 +22,30 @@ class InputVcfReader(object):
         self.min_read_quality = min_read_quality
         self.trust = trust
         self.cc_input = cc_input
-
         self.reads_proxy = None
+        #self.reads_proxy = pysam.AlignmentFile(self.bam_file, "rb")    
+        self.encoding_table = {'A':1, 'T':2, 'C':3, 'G':4, 1:'A', 2:'T', 3:'C', 4:'G', 0:' ', ' ':0, 6:'-', '-':6}
+
+        #self.reads_proxy = None
         self.logging = logging
 
-    ## remove
-    def _load_haplo_fr_vcf(self, haplo_vcf_file):
-        """
-        Read standard GATK VCF
-        """
-        haplo_list = list()
-        haplo_file_f = haplo_vcf_file
-        if isinstance(haplo_vcf_file, str):
-            haplo_file_f = open(haplo_vcf_file, 'r')
+    def reset(self, haplo_file, bam_file, min_base_quality, min_read_quality, cc_input=None, indels=False, trust=True):
+        self.haplo_file = haplo_file
+        self.bam_file = bam_file
+        self.indels = indels
+        self.min_base_quality = min_base_quality
+        self.min_read_quality = min_read_quality
+        self.trust = trust
+        self.cc_input = cc_input
+        #self.reads_proxy = pysam.AlignmentFile(self.bam_file, "rb")    
+        self.encoding_table = {'A':1, 'T':2, 'C':3, 'G':4, 1:'A', 2:'T', 3:'C', 4:'G', 0:' ', ' ':0, 6:'-', '-':6}
 
-        # error hanlder for multiallelic
-        for line in haplo_file_f:
-            e = line.strip().split("\t")
-            if line[0] != '#':
-                chr_name     = e[0]
-                variant_loc  = int(e[1])
-                ref          = e[3]
-                alt          = e[4]
-                ref_len      = len(ref)
-                alt_len      = len(alt)
-                         
-                if ref_len == 1 and alt_len == 1:
-                    haplo_list.append([chr_name, variant_loc, 0, 'snp', ref, ref, alt])
-                elif self.indels == True:
-                    if(ref_len > alt_len):
-                        #deletion
-                        haplo_list.append([chr_name, variant_loc, ref_len - alt_len,  'deletion', ref, ref, alt])
-                    else:
-                        #insertion
-                        haplo_list.append([chr_name, variant_loc, alt_len - ref_len, 'insertion', ref, ref, alt])
- 
+        #self.reads_proxy = None
+        self.logging = logging
 
-        return haplo_list
 
-    def get_readmtx(self):
+
+    def get_readmtx(self, timer):
         """
         Args:
 
@@ -96,42 +72,43 @@ class InputVcfReader(object):
             phase_variants_called_list = [(ref_major, called_h1, called_h2), (ref_major, called_h1, called_h2), ... ]
         """
 
-        logging.info("start _load_haplo")
-        if self.cc_input == None:
-            phase_input = self._load_haplo_fr_vcf(self.haplo_file)
-        else:
-            phase_input = self.cc_input
+        #logging.info("start _load_haplo")
+        phase_input = self.cc_input
 
-        print(phase_input)
-
+        timer.start('readmtx_init')
         phase_variants_location_list = list(map(lambda x:x[1], phase_input))
         phase_variants_called_list = list(map(lambda x:(x[4],x[5],x[6]), phase_input))
         phase_fragments_dict = OrderedDict()
-        logging.info("start _read_variance")
+        timer.stop('readmtx_init')
+        #logging.info("start _read_variance")
 
         # Open Pysam 
-        self.reads_proxy = pysam.AlignmentFile(self.bam_file, "rb")
+        if self.reads_proxy == None:
+            self.reads_proxy = pysam.AlignmentFile(self.bam_file, "rb")
 
+        #timer.start('read_variance')
         # Read from Pysam
         for vd in phase_input:
-            self._read_variance(vd[V_DESC_CRHOM], vd[1], vd[2], vd[3], phase_fragments_dict, (vd[5], vd[6]), self.trust)
+            self._read_variance(vd[0], vd[1], vd[2], vd[3], phase_fragments_dict, (vd[5], vd[6]), self.trust, timer)
+        #timer.stop('read_variance')
 
         # Close Pysam
-        self.reads_proxy.close()
+        #self.reads_proxy.close()
 
+        timer.start('clear_matrix')
         self._clear_matrix(phase_fragments_dict)
+        timer.stop('clear_matrix')
 
-
-        print(len(phase_fragments_dict), len(phase_variants_location_list))
-        logging.info("start _read_matrix")
+        #logging.info("start _read_matrix")
         #read_matrix = np.zeros(shape=(len(phase_fragments_dict), len(phase_variants_location_list)), dtype='int')
         read_matrix = lil_matrix((len(phase_fragments_dict), len(phase_variants_location_list)), dtype=np.int8)
-        #print(phase_variants_location_list)
 
-        logging.info("start _produce_var_matrix")
+        #logging.info("start _produce_var_matrix")
+        timer.start('produce_var_matrix')
         phase_fragments_range_list = self._produce_var_matrix(read_matrix, phase_fragments_dict, phase_variants_location_list, InputVcfReader.encoding_table)
+        timer.stop('produce_var_matrix')
 
-        del phase_input 
+        #del phase_input 
         return read_matrix, phase_fragments_dict, phase_fragments_range_list, phase_variants_location_list, InputVcfReader.encoding_table, phase_variants_called_list
 
 
@@ -152,17 +129,19 @@ class InputVcfReader(object):
         for x in noninfo:
             del phase_fragments_dict[x]
 
-    def _read_variance(self, chrom, loci, extend_len, var_type, phase_fragments_dict, ref, trust):
+    def _read_variance(self, chrom, loci, extend_len, var_type, phase_fragments_dict, ref, trust, timer):
         """
         Read raw NGS data of each variants from sam file.
 
         """
 
         removed_set = set()
-        print(loci, ref)
         # (loci - 1) in array = loci in seqs
+        timer.start('fetch')
         for read in self.reads_proxy.fetch(chrom, loci - 1, loci):
+            timer.stop('fetch')
             #read quality filter
+
             if(read.mapping_quality < self.min_read_quality):
                 continue
 
@@ -191,11 +170,30 @@ class InputVcfReader(object):
                 read_id = read.query_name + "_" + str(read.reference_start)
 
 
+            timer.start('aligned_pairs')
+
             allele = ''
             read_indel_type = 'S'
             aligned_pairs = read.get_aligned_pairs()
+            aligned = [i[1] for i in aligned_pairs]
+            try:
+
+                idx = aligned.index(loci - 1)
+                observed = read.seq[aligned_pairs[idx][0]]
+
+                if(aligned_pairs[idx][0] == None):
+                    allele = '-'
+                elif(observed not in ref and trust == True):
+                    allele = '-'
+                else:
+                    allele = observed
+
+            except (ValueError, TypeError) as error:
+                allele = '-'
+                pass
+            '''
+
             for aligned in aligned_pairs:
-                #slow?? 
                 if aligned[1] == loci - 1:
                     if(aligned[0] == None):
                         allele = '-'
@@ -206,7 +204,11 @@ class InputVcfReader(object):
                     if(allele not in ref and trust == True):
                         allele = '-'
                         break
+            '''
 
+            timer.stop('aligned_pairs')
+
+            timer.start('phase_fragments_dict')
             if(read_id in phase_fragments_dict):
                 #overlapping paired-end, but different in target allele
                 overlapped = False
@@ -221,11 +223,15 @@ class InputVcfReader(object):
                     phase_fragments_dict[read_id].append((chrom, loci, allele, read_indel_type))
             else:
                 phase_fragments_dict[read_id] = [(chrom, loci, allele, read_indel_type)]
-
+            timer.stop('phase_fragments_dict')
+            timer.start('fetch')
+        timer.stop('fetch')
         #remove unqualified reads
+        timer.start('removed_set')
         for i in removed_set:
             del phase_fragments_dict[i]
-
+        timer.stop('removed_set')
+ 
         return
 
     def _produce_var_matrix(self, read_matrix, phase_fragments_dict, phase_variants_location_list, encoding_table):
